bc6bac4674d9a84c67bff32915e1a506b322511c
Muhussai_Workstation <hussainmaheen88@gmail.com>
2023-07-09 18:08:04 +0530

    Listing creation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image'
 import Background from './components/Hero/Background'
 import MiddleContent from './components/Navbar/MiddleContent'
+import { GiSailboat } from 'react-icons/gi'
 
 export default function Home() {
   return (
     <>
   <Background/>
-  <div className='flex flex-row'>
-    <h3>Scenic Routes</h3>
-    <h3>200+ houseboats</h3>
-  </div>
+
     </>
   )
 }
