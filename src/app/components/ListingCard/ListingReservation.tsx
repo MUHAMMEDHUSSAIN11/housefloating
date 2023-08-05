28a4098cbbf621553b506875559c636f3381dd7f
Muhussai_Workstation <hussainmaheen88@gmail.com>
2023-08-05 14:09:26 +0530

    Custom calendar

--- src/app/components/ListingCard/ListingReservation.tsx
+++ src/app/components/ListingCard/ListingReservation.tsx
@@ -20,7 +20,7 @@ interface ListingReservationProps {
 
 const ListingReservation: React.FC<ListingReservationProps> = ({price,totalPrice,date,onChangeDate,onSubmit,disabled,disabledDates}) => {
 
-  console.log(disabledDates);
+
   return ( 
     <div className=" bg-white  rounded-xl  border-[1px] border-neutral-200  overflow-hidden">
       <div className="flex flex-row items-center gap-1 p-4">
