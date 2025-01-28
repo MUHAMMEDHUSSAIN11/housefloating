74389dfd6e80c82b71dd7dfcb8e31d1b18bc27f6
Muhussai_Workstation <hussainmaheen88@gmail.com>
2025-01-29 02:27:42 +0530

    fix : title size and robots.txt added

--- src/app/components/Hero/Background.tsx
+++ src/app/components/Hero/Background.tsx
@@ -11,7 +11,7 @@ const Background = () => {
       <img src="/images/try.jpg" alt="Hero background image" className="absolute inset-0 object-cover w-full h-full"/>
       <div className="absolute inset-0 bg-black/20 flex flex-col justify-center items-center lg:items-start lg:pl-16">
         <div className="text-center mt-36 lg:text-left">
-          <h1 className="text-white sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl font-sans font-semibold leading-tight">
+          <h1 className="text-white sm:text-5xl md:text-4xl lg:text-5xl xl:text-5xl font-sans font-semibold leading-tight">
             Discover Alleppey's<br/> Stunning Houseboats Online
           </h1>
         </div>
