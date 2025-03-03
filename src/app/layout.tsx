fb269e3d70f6ef482e59a7bda3cb3267ca545b9a
Muhussai_Workstation <hussainmaheen88@gmail.com>
2025-03-03 20:03:28 +0530

    Added : Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,12 @@ import RentModal from './components/Modals/RentModal'
 import NextTopLoader from 'nextjs-toploader';
 import ScrollToTopButton from './components/Misc/ScrolltoTop'
 import { Toaster } from 'react-hot-toast'
+import Script from 'next/script'
 
 export const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-  preload:true
+  preload: true
 })
 
 
@@ -24,7 +25,34 @@ export default async function RootLayout({ children, }: { children: React.ReactN
 
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google Tag Manager (GTM) - Head Script */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PDC8KBVM');
+            `,
+          }}
+        />
+      </head>
+
       <body className={inter.className}>
+        {/* ✅ Google Tag Manager (GTM) - NoScript (For users with JS disabled) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PDC8KBVM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Toaster
           position="top-center"
           reverseOrder={false}
