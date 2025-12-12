48a20525a40d1d09131e74761db237c00ae2fa46
Muhammed Nuaim <nuaimnoor69@gmail.com>
2025-12-12 10:59:44 +0530

    chore: update dependencies and devDependencies to latest versions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // Only allow images from Firebase Storage
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
-
-module.exports = {
-    images: {
-      domains: ['firebasestorage.googleapis.com'],
-    },
-  };
